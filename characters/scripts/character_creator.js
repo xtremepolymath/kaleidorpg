70232e9466ca0fa3333a7f1d92d9ba2c2078fb67
xtremepolymath <46331086+xtremepolymath@users.noreply.github.com>
2020-05-02 19:44:28 -0700

    Create character_creator.js

---
+++ characters/scripts/character_creator.js
@@ -0,0 +1,9 @@
+function switchRacePanel(race){
+    var racePans = document.getElementsByClassName("race_panel");
+    for(var i=0;i<racePans.length;i++){
+        racePans[i].style.height = "0px";
+    }
+
+    document.getElementById(race).style.height = "680px";
+
+}
